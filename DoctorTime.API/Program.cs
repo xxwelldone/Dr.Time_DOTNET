bd1f2349619c74cc4acb1b80daa5de0b2ef35fb9
Wesley Menezes <wesleymenezesdnovaes@gmail.com>
2025-02-06 13:32:06 -0300

    Fix: Fix Swagger exposure for Render

--- DoctorTime.API/Program.cs
+++ DoctorTime.API/Program.cs
@@ -97,11 +97,14 @@ namespace DoctorTime.API
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            //if (app.Environment.IsDevelopment())
+            //{
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI();
+            //}
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
             app.UseAuthentication();
